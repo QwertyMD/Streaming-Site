d9da5bc25ff4e5626a6740148e1fcb976395fcce
Aaryan Dhakal <127584885+QwertyMD@users.noreply.github.com>
2025-02-15 14:37:42 +0545

    Change UI

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-const display = document.querySelector(".content");
-const wait = document.querySelector(".pending");
+const searchButton = document.querySelector("#searchButton");
+const searchInput = document.querySelector("#searchInput");
+const movieContainer = document.querySelector("#movieContainer");
 
-document.getElementById("result").addEventListener("keypress", (e) => {
-  const input = document.getElementById("result").value;
-  if (e.key === "Enter") {
-    display.innerHTML = ``;
-    wait.innerHTML = `<h1>Searching...</h1>`;
-    async function getDetails() {
-      const res = await fetch(
-        `https://imdb.iamidiotareyoutoo.com/search?q=${input}`,
-        {
-          headers: {
-            Accept: "application/json",
-          },
-        }
-      );
-      const data = await res.json();
-      if (data.description && data.description.length > 0) {
-        for (let i = 0; i < data.description.length; i++) {
-          console.log(data.description[i]);
-          let title = data.description[i]["#TITLE"];
-          let url = data.description[i]["#IMDB_URL"];
-          const img = new Image();
-          img.src = data.description[i]["#IMG_POSTER"];
-          img.onload = () => {
-            wait.innerHTML = ``;
-            display.insertAdjacentHTML(
-              "beforeend",
-              `<div class="items">
-                <img src="${img.src}" alt="pic">
-                <a href="${url}" target="_blank">${title}</a>
-            </div>`
-            );
-          };
-        }
-      } else {
-        wait.innerHTML = `<h1>Nothing Found :(</h1>`;
-      }
-    }
-    getDetails();
-  }
+searchButton.addEventListener("click", () => {
+  const searchInputValue = searchInput.value;
+  getMovie(searchInputValue);
 });
+
+async function getMovie(searchInputValue) {
+  try {
+    movieContainer.innerHTML = ``;
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=39754966&s=${searchInputValue}`
+    );
+    const data = await response.json();
+    console.log(data);
+
+    if (data.Response === "True") {
+      data.Search.forEach((element) => {
+        movieContainer.insertAdjacentHTML(
+          "beforeend",
+          `<a target="_blank" href="https://www.imdb.com/title/${element.imdbID}/" class="movie-card">
+             <img src="${element.Poster}" alt="${element.Title}">
+             <h3>${element.Title}</h3>
+           </a>`
+        );
+      });
+    } else {
+      movieContainer.innerHTML = `<p>Movie not found.</p>`;
+    }
+  } catch (error) {
+    movieContainer.innerHTML = `<p>Error fetching movie data.</p>`;
+  }
+}
