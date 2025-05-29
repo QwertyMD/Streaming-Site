532aa4a6ee4af9e30763b22d9dd889b01e8208e5
QwertyMD <aaryandhakal2020@gmail.com>
2025-05-29 19:20:31 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-primeblue to-primeblack">
+      OpenFlix
+    </div>
+  );
+};
+
+export default App;
