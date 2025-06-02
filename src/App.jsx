ac5c8fd532dee3c21e7b806fa85c1e5216c04afa
QwertyMD <aaryandhakal2020@gmail.com>
2025-06-02 11:49:51 +0545

    switch to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "@/components/Layout.jsx";
 import Landing from "@/components/Landing.jsx";
 import NotFound from "@/pages/NotFound.jsx";
@@ -14,7 +14,7 @@ import WatchShow from "./pages/WatchShow";
 const App = () => {
   return (
     <CollectionsProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Landing />} />
@@ -27,7 +27,7 @@ const App = () => {
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </CollectionsProvider>
   );
 };
